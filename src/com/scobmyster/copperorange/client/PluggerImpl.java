7bd3d936255165942921f544a328a73d23f25f43
Scobmyster <jaa.scobbie@googlemail.com>
2017-11-19 09:22:10 +0000

    Removal of log stuff
    Housekeeping!!

--- src/com/scobmyster/copperorange/client/PluggerImpl.java
+++ src/com/scobmyster/copperorange/client/PluggerImpl.java
@@ -4,7 +4,6 @@ import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.scobmyster.copperorange.logging.LogManager;
 import com.scobmyster.copperorange.client.process.ProcessModel;
 import com.scobmyster.copperorange.client.process.client.RotaAddRowImpl;
 import com.scobmyster.copperorange.client.process.client.RotaRemoveRowImpl;
